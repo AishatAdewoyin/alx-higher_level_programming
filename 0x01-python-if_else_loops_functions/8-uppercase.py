7651e3a8e801abbb8ae93e3bfa323fe016677fc5
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-07 00:05:03 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/bin/bash/python3
 def uppercase(s):
+    uppercase_str = ""
     for c in s:
         if ord(c) >= 97 and ord(c) <= 122:
-            offset = 32
-        else:
-            offset = 0
-        print("{}".format(chr(ord(c) - offset)), end="")
-    print()
+            c = chr(ord(c) - 32)
+        uppercase_str += c
+    print(uppercase_str)
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
