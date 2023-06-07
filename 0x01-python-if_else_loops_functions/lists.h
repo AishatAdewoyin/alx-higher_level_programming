7920b88f3e36aa5a9fd042226c800d19664f5feb
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-07 23:53:37 +0100

    Update lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,21 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
 
 /**
- * struct listint_s - linked list
+ * struct listint_s - linked list.
  * @n: int
- * @next: points to next node
+ * @next: next node ptr
+ *
+ * Description: node struct for list.
+ *
  */
+
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-listint_t *create_node(int number);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
