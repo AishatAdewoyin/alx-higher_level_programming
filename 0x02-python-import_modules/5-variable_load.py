77d1eab74d47d3338ee6e9b67fa41b905517f5c2
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-10 00:53:17 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    names = [n for n in dir(hidden_4) if not n.startswith("__")]
-    print("\n".join(names))
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import x
+    print(x)
