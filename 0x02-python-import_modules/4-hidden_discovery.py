b24cbfbda8bd89a82f68cdd539d5a686bdce0d9d
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-10 00:51:28 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-
-    names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    print('\n'.join(names))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
