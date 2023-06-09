da20289ca4d7111948f37ac24b1d7d5567ac30b5
Aishat452 <aishadeperfectly@gmail.com>
2023-06-08 17:39:35 -0700

    variable load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    name = [n for n in dir(hidden_4) if not n.startswith("__")]
-print("\n".join(names))
+    import hidden_4
+    
+    names = [n for n in dir(hidden_4) if not n.startswith("__")]
+    print("\n".join(names))
