b1ac64a52795fec3f1682df01fb563d87801754c
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-11 00:30:26 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import x
-    print(x)
+    from variable_load_5 import a
+    print(a)
