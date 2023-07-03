1a08c924b95656b9a0320e3e96f7b3b5c83c7ad4
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-07-03 19:40:22 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    indexes = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-        except IndexError:
-            break
-        else:
-            indexes += 1
-    print()
-    return indexes
+    counted_list = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            counted_list += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return counted_list
