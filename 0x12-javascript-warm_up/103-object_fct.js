4bc947ce58600ec40e49fa01fd0ea4c4e74378f9
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-09-12 21:36:10 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
-myObject.incr = function () {
-  this.value++;
-};
-
-console.log(myObject);
+myObject.incr = () => { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
