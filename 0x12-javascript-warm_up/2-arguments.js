37c9acb408bf2c8f6cdd80e3755c1b17a1f0f953
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-09-12 20:01:23 +0100

    javascript is amazing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let numberOfArguments = process.argv.length;
+
+if (numberOfArguments == 0){
+	console.log('No arguments');
+} else if (numberOfArguments == 1){
+	console.log('Argument found');
+} else{
+	console.log("Arguments found");
+}
